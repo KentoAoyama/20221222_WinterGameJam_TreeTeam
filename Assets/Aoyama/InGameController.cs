25d5337ad0cea3730f990c242e7d3a0041e5d1bc
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2022-12-22 10:50:24 +0900

    [add]ちょっと追加

---
+++ Assets/Aoyama/InGameController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class InGameController : MonoBehaviour
+{
+    [SerializeField]
+    private Text _scoreText;
+
+    [SerializeField]
+    private Text _timeText;
+
+    [SerializeField]
+    private CinemachineVirtualCamera _cinemachine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.Cinemachine = _cinemachine;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SetText()
+    {
+        _scoreText.text = _score.ToString();
+        _timeText.text = _time.ToString();
+    }
+}
