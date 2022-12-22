705434aba219dd35de8afd49498198f2c316aae0
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-12-22 12:57:13 +0900

    餅、爆弾仮完成

---
+++ Assets/Adachi/Scripts/BitterOrangeController.cs
@@ -0,0 +1,38 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ãæñ›ÇÃè„Ç…ÇÃÇπÇÈûÚÅiÇæÇ¢ÇæÇ¢Åj
+/// </summary>
+public class BitterOrangeController : ItemBase
+{
+    private void Awake()
+    {
+        OnMove();
+    }
+
+    protected override void OnBecameInvisible()
+    {
+        _isMoving = false;
+        Destroy(gameObject);
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == _playerTag)
+        {
+            _isMoving = false;
+        }
+    }
+
+    async protected override void OnMove()
+    {
+        while (_isMoving)
+        {
+            transform.Translate(0f, -_speed, 0f);
+            await UniTask.NextFrame();
+        }
+    }
+}
